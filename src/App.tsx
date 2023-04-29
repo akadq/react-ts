387323572b99d7bf9ab8ea0919194d9c44bc65c9
akadq <a.kadkalo@alberto.com.ua>
2023-04-29 21:49:39 +0300

    started a new ts project from hw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [count, setCount] = useState(0);
+    function handleClick() {
+        // direction === 'plus' ? setCount(count +1) : setCount(count - 1);
+        setCount(count +1)
+    }
+       return (
+        <div>
+            <Button count={count} onClick={handleClick}/>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
+
+// export interface ICommento {
+//     sky: string;
+//     bridge: string;
+//     year: number;
+// }
