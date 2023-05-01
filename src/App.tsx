11232763db5cf043c74bbe79eecadc9be39a38a1
akadq <a.kadkalo@alberto.com.ua>
2023-05-01 11:25:49 +0300

    routes ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React, {useState} from 'react';
-import Button from "./components/Button";
 
 const App = () => {
-    const [count, setCount] = useState(0);
-    function handleClick() {
-        // direction === 'plus' ? setCount(count +1) : setCount(count - 1);
-        setCount(count +1)
-    }
+
        return (
         <div>
-            <Button count={count} onClick={handleClick}/>
+
         </div>
     );
 };
